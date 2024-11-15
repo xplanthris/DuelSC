2ac4ac187fa55d903d32bd65b93ad54c1eb6285c
Syed Muhammad Shuja Haider <xplanthris@gmail.com>
2024-11-15 16:13:05 +0300

    Set to foreground aka. always on top
    Yeah so I realized this after publishing my first release....

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    // Set to foreground
+    w.setWindowFlags(w.windowFlags() | Qt::WindowStaysOnTopHint);
+
     w.show();
     return a.exec();
 }
